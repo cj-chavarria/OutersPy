499e16111151787a2409f23bbc2e7628f4374754
Carlos Chavarria <andriodcj131011@gmail.com>
2026-09-20 22:40:10 -0600

    test parse horizon text to extract table of Elements

--- dev/try_nasa_apis.py
+++ dev/try_nasa_apis.py
@@ -1,52 +1,105 @@
 import marimo
 
-__generated_with = "0.24.0"
-app = marimo.App()
+__generated_with = "0.24.2"
+app = marimo.App(width="full")
 
 
 @app.cell
 def _():
-    from typing import Any
-
+    import json
+    import csv
+    from io import StringIO
     import httpx
-    from httpx._models import Response
+    import pandas as pd
 
-    return Any, Response, httpx
+    return StringIO, httpx, pd
 
 
 @app.cell
 def _():
-    JPL_API_URL= "https://ssd-api.jpl.nasa.gov/sbdb_query.api"
-    params: dict[str, str] = {
+    JPL_API_URL = "https://ssd-api.jpl.nasa.gov/sbdb_query.api"
+    params_jpl: dict[str, str] = {
         "fields": "full_name,epoch,e,a,q,i,om,w",
-        "sb-class": "COM"
+        "sb-class": "COM",
     }
-    return JPL_API_URL, params
-
-
-@app.cell
-def _(Any, JPL_API_URL, Response, httpx, params: dict[str, str]):
-    response: Response = httpx.get(url=JPL_API_URL, params=params)
-    response.raise_for_status()
-
-    data: Any = response.json()
     return
 
 
 @app.cell
-def _(Response, httpx):
-    response2: Response = httpx.get(
-        url="https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'"
-    )
-    response2.raise_for_status()
-
-    data2: str = response2.text
-    return (data2,)
+def _():
+    HORIZON_API = "https://ssd.jpl.nasa.gov/api/horizons.api"
+    params_horizon: dict[str, str] = {
+        "format": "json",
+        "COMMAND": "499",
+        "EPHEM_TYPE": "ELEMENTS",
+        "START_TIME": "2026-09-19",
+        "STOP_TIME": "2026-09-30",
+        "STEP_SIZE": "1d",
+        "CENTER": "@10",
+        "CSV_FORMAT": "YES",
+    }
+    return HORIZON_API, params_horizon
 
 
 @app.cell
-def _(data2: str):
-    print(data2)
+def _(HORIZON_API, httpx, params_horizon: dict[str, str]):
+    response = httpx.get(url=HORIZON_API, params=params_horizon)
+    response.raise_for_status()
+
+    data = response.json()
+    return (data,)
+
+
+@app.cell
+def _(data):
+    result = data.get("result")
+    print(result)
+    return (result,)
+
+
+@app.cell
+def _():
+    columns = ['JDTDB','Calendar Date (TDB)','EC','QR','IN','OM','W','Tp','N','MA','TA','A','AD','PR']
+    return (columns,)
+
+
+@app.cell
+def _(result):
+    if "$$SOE" in result and "$$EOE" in result:
+        table = result.split("$$SOE\n")[1].split("\n$$EOE")[0]
+    else:
+        print("No table")
+    return (table,)
+
+
+@app.cell
+def _(StringIO, columns, pd, table):
+    df = pd.read_csv(StringIO(table), header=None, names=columns, index_col=False)
+    df
+    return
+
+
+@app.cell
+def _():
+    from astroquery.jplhorizons import Horizons
+
+    obj = Horizons(
+        id="499",
+        location="@10",
+        epochs={"start": "2026-09-19", "stop": "2026-09-30", "step": "1d"},
+    )
+    return (obj,)
+
+
+@app.cell
+def _(obj):
+    elem = obj.elements()
+    return (elem,)
+
+
+@app.cell
+def _(elem):
+    elem.to_pandas()
     return
 
 
